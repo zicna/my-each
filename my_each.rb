2240f43b527db9691a155e17bdb42e0967747732
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-10 22:33:31 -0600

    commit tree 52078cfb7931d7713608641029d3041c49385dc9

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    
+    return array
+  end
 end
+#my_each([1, 2, 3, 4, 5, 6])
